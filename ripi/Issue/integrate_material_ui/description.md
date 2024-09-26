14eed00a3e3a651d40c24cd8119775975964e376
Ismael Pamplona <ismaelpamplona@hotmail.com>
2024-09-26 17:07:01 -0300

    (up) Issue #integrate_material_ui.

--- ripi/Issue/integrate_material_ui/description.md
+++ ripi/Issue/integrate_material_ui/description.md
@@ -1,1 +1,7 @@
 # integrate_material_ui (Issue)
+
+- [x] Install the core MUI package and icons package using npm.
+- [x] Set up Material-UI theming:
+  - [x] Create a `theme.ts` file in a `src/theme` directory.
+  - [x] Configure a basic MUI theme with primary and secondary colors.
+  - [x] Wrap the application with the `ThemeProvider` in `AppRouter.tsx`.
--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -1,15 +1,19 @@
+import { ThemeProvider } from '@mui/material/styles';
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import RegisterUser from './pages/RegisterUser';
+import theme from './theme/theme';
 
 const AppRouter = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/register" element={<RegisterUser />} />
-      </Routes>
-    </Router>
+    <ThemeProvider theme={theme}>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/register" element={<RegisterUser />} />
+        </Routes>
+      </Router>
+    </ThemeProvider>
   );
 };
 
